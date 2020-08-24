4cc819e5949e116cc652fbb49691b27829ced6bb
Jeff Dinsmore <hello@jeffdinsmore.com>
2020-08-24 15:01:14 -0700

    updated the readme document. It still needs specs for the web Co-authored-by: Amanda Otero <mandystar018@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,28 @@
+// Business Logic
+function toDoList() {
+  this.task = [];
+  this.currentId = 0;
+}
+
+toDoList.prototype.addTask = function(taskItems) {
+  taskItems.id = this.assignId();
+  this.task.push(taskItems);
+}
+
+
+
+function List(name, description, date) {
+  this.name = name;
+  this.description = description;
+  this.date = date;
+}
+
+
+
+
+
+
+
 $(document).ready(function() {
   
   
